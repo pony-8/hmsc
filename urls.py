53f115f52331f59c738cfaec335a580a5d6443bd
pony-8 <2604665482@qq.com>
2020-04-27 22:05:33 +0800

    整个user模块完成，作业创建账号

--- urls.py
+++ urls.py
@@ -2,6 +2,7 @@ from application import app
 from web.controllers.user.User import router_user
 from web.controllers.static import route_static
 from web.controllers.index import route_index
+from web.controllers.account.Account import router_account
 
 #拦截路由
 from web.interceptos.AuthInterceptor import *
@@ -9,3 +10,4 @@ from web.interceptos.AuthInterceptor import *
 app.register_blueprint(router_user,url_prefix="/user")
 app.register_blueprint(route_index,url_prefix="/")
 app.register_blueprint(route_static,url_prefix="/static")
+app.register_blueprint(router_account,url_prefix="/account")
