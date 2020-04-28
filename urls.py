d8b5c2248d4bc1a0549aaa02e02cc339c003c237
pony-8 <2604665482@qq.com>
2020-04-28 19:16:52 +0800

    搜索，更新商品管理模板

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from application import app
+from web.controllers.goods.Goods import router_goods
 from web.controllers.user.User import router_user
 from web.controllers.static import route_static
 from web.controllers.index import route_index
@@ -9,5 +10,6 @@ from web.interceptos.AuthInterceptor import *
 
 app.register_blueprint(router_user,url_prefix="/user")
 app.register_blueprint(route_index,url_prefix="/")
+app.register_blueprint(router_goods,url_prefix="/goods")
 app.register_blueprint(route_static,url_prefix="/static")
 app.register_blueprint(router_account,url_prefix="/account")
--- web/controllers/account/Account.py
+++ web/controllers/account/Account.py
@@ -1,20 +1,46 @@
 from flask import Blueprint,request,redirect,jsonify
 
-from application import db
-from common.libs.Helper import ops_render,getCurrentDate
+from application import db,app
+from common.libs.Helper import ops_render,getCurrentDate,iPagination
 from common.libs.UrlManager import UrlManager
 from common.libs.user.userService import UserService
-from common.models.User import User
+from common.models.User import User,db
+from sqlalchemy import or_
+
 
 router_account = Blueprint("account_page",__name__)
 
 @router_account.route('/index')
 def index():
     resp_data = {}
-    list = User.query.all()
-    resp_data['list'] = list 
-    return ops_render('account/index.html',resp_data)
+    req = request.values
+    page = int(req['p']) if ('p' in req and req['p']) else 1
+    query = User.query
+    if 'status' in req and int(req['status']) > -1:
+        query = query.filter(User.status == int(req['status']))
+    if 'mix_kw' in req:
+        rule = or_( User.nickname.ilike("%{0}%".format(req['mix_kw'])),User.mobile.ilike("%{0}%".format(req['mix_kw'])) )
+        query = query.filter(rule)
+    
+    params = {
+        'total':query.count(),
+        'page':page,
+        'page_size':app.config['PAGE_SIZE'],
+        'url':request.full_path.replace("&p={}".format(page),"")
+    }
+    # 分页的三大关键字
+    pages = iPagination(params)
+    offset = (page-1)*app.config['PAGE_SIZE']
+    limit = page*app.config['PAGE_SIZE']
 
+    list = query.all()[offset:limit]
+    resp_data['list'] = list 
+    resp_data['status'] = {
+        "1":"正常",
+        "0":"已删除"
+    }
+    resp_data['pages'] = pages
+    return ops_render('account/index.html',resp_data)
 @router_account.route('/info')
 def info():
     resp_data = {}
@@ -94,9 +120,48 @@ def set():
     model_user.mobile = mobile
     model_user.email = email
     model_user.login_name = login_name
+    model_user.login_pwd = UserService.generatePwd(login_pwd,model_user.login_salt)
 
     model_user.updated_time = getCurrentDate()
     
     db.session.add(model_user)
+    db.session.commit()
+    return jsonify(resp)
+
+@router_account.route('removeOrRecover',methods=["GET","POST"])
+def removeOrRecover():
+    resp = {
+        'code':200,
+        'msg':'操作成功',
+        'data':{}
+    }
+    req = request.values
+    id = req['id'] if 'id' in req else 0
+    acts = req['acts'] if 'acts' in req else ''
+    if not id:
+        resp['code'] = -1
+        resp['msg'] = "未操作操作账号"
+        return jsonify(resp)
+    if acts not in ['remove','recover']:
+        resp['code'] = -1
+        resp['msg'] = "操作有误"
+        return jsonify(resp)
+    
+    user_info = User.query.filter_by(uid=id).first()
+    if not user_info:
+        resp['code'] = -1
+        resp['msg'] = "指定的账号不存在"
+        return jsonify(resp)
+    if user_info and user_info.uid == 1:
+        resp['code'] = -1
+        resp['msg'] = "该用户为Bruce，不能操作账号"
+        return jsonify(resp)
+    if acts == "remove":
+        user_info.status = 0
+    elif acts == "recover":
+        user_info.status = 1
+        
+    user_info.updated_time = getCurrentDate()
+    db.session.add(user_info)
     db.session.commit()
     return jsonify(resp)
