faf731341721b13b242cb8f7c3991d4ce97b0801
pony-8 <2604665482@qq.com>
2020-04-29 20:34:16 +0800

    会员评论，会员管理！！

--- urls.py
+++ urls.py
@@ -4,6 +4,7 @@ from web.controllers.user.User import router_user
 from web.controllers.static import route_static
 from web.controllers.index import route_index
 from web.controllers.account.Account import router_account
+from web.controllers.member.Member import router_member
 
 #拦截路由
 from web.interceptos.AuthInterceptor import *
@@ -11,5 +12,6 @@ from web.interceptos.AuthInterceptor import *
 app.register_blueprint(router_user,url_prefix="/user")
 app.register_blueprint(route_index,url_prefix="/")
 app.register_blueprint(router_goods,url_prefix="/goods")
+app.register_blueprint(router_member,url_prefix="/member")
 app.register_blueprint(route_static,url_prefix="/static")
 app.register_blueprint(router_account,url_prefix="/account")
